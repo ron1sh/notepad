608e363541e5c5bd0269f3586c3d6995c67848b5
ron1sh <ronishmainali9@gmail.com>
2026-03-10 18:12:13 +0545

    this is the main change now

--- src/components/Inputitems.jsx
+++ src/components/Inputitems.jsx
@@ -5,11 +5,55 @@ export default function Inputitems(){
     const [texts,settexts]=useState("");
     const titletextAreaRef = useRef(null);
     const contenttextAreaRef=useRef(null);
+    const [isSaved, setIsSaved] = useState(false);
+    const [isempty,setisempty]=useState(false);
     const navigate=useNavigate();
+    const [checkserver,setcheckserver]=useState(false);
     const hideScrollbar = "scrollbar-hide [ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden";
 
 
-  const handleChange = (e) => {
+    const postingdata= async ()=>{
+        if(!texts && !title){
+            setisempty(true);
+            setTimeout(()=>setisempty(false),2000)
+            return;
+        }
+        try{
+        const response= await fetch("/api/newnote",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                title:title,
+                content:texts
+            })
+        });
+        if (response.status===200){
+            console.log("successfully saved!");
+            setIsSaved(true);
+            setisempty(false);
+            setcheckserver(false);
+            setTimeout(() => {
+                    setIsSaved(false);
+                    navigate("/", { replace: true });
+                }, 1200);
+        } else {
+            setcheckserver(true);
+            setTimeout(() => {
+                    setcheckserver(false);
+                    navigate("/", { replace: true });
+                }, 2000);
+
+            console.error("Failed to save note");
+        }
+    } catch(err){
+        console.log(err);
+
+    }
+    }
+
+    const handleChange = (e) => {
     if(e.target.id==="titletext"){
     settitle(e.target.value);
     titletextAreaRef.current.style.height = "auto";
@@ -21,16 +65,31 @@ export default function Inputitems(){
     }
   };
     return(
-        <div className="flex items-center justify-center min-h-screen w-screen]">
+        <div className="flex items-center justify-center min-h-screen w-screen relative ">
+            {isSaved && (
+                <div className="fixed top-10 left-1/2 -translate-x-1/2 z-[100] bg-green-600 text-white px-6 py-3 rounded-xl shadow-2xl font-bold animate-bounce">
+                    ✅ Note saved successfully!
+                </div>
+            )}
+            {isempty && 
+                <div className="fixed top-10 left-1/2 -translate-x-1/2 z-[100] bg-red-600 text-white px-6 py-3 rounded-xl shadow-2xl font-bold animate-bounce">
+                    Empty data cannot be saved
+                </div>}
+            {checkserver && 
+                <div className="fixed top-10 left-1/2 -translate-x-1/2 z-[100] bg-blue-600 text-white px-6 py-3 rounded-xl shadow-2xl font-bold animate-bounce">
+                    Server is not responsing at the moment. you will be redirected to "Home"!
+                </div>}
+        
             <div className={`rounded-2xl shadow-[0_20px_50px_rgba(0,0,0,0.1)] flex flex-col justify-center p-3 min-w-[75vw] max-w-[75vw] min-h-[80vh] max-h-[80vh]  gap-4 overflow-scroll px-12 ${hideScrollbar}`}>
-                <textarea name="" id="titletext" ref={titletextAreaRef} value={title} onChange={handleChange} placeholder="Title......." className={`resize-none outline-none border-none bg-transparent shadow-[0_4px_2px_-2px_rgba(0,0,0,0.2)] font-bold text-3xl pt-2  ${hideScrollbar}`}></textarea>
-                <textarea name="" id="contenttext" ref={contenttextAreaRef} value={texts} onChange={handleChange} placeholder="Content...." className={`resize-none outline-none border-none bg-transparent flex flex-1 text-2xl  ${hideScrollbar}`}></textarea>
-                <div className="flex justify-end hover:cursor-pointer">
-                    <button className="border text-2xl font-bold border-black px-8 py-2 hover:cursor-pointer hover:bg-[#5e8c6b] rounded-2xl" onClick={() =>{ 
-                        console.log(title);
-                        console.log(texts);
-                        navigate("/",{replace:true})
-                        }}>Post</button>
+                <textarea name="title" id="titletext" ref={titletextAreaRef} value={title} onChange={handleChange} placeholder="Title......." className={`resize-none outline-none border-none bg-transparent shadow-[0_4px_2px_-2px_rgba(0,0,0,0.2)] font-bold text-3xl pt-2  ${hideScrollbar}`}></textarea>
+                <textarea name="content" id="contenttext" ref={contenttextAreaRef} value={texts} onChange={handleChange} placeholder="Content...." className={`resize-none outline-none border-none bg-transparent flex flex-1 text-2xl  ${hideScrollbar}`}></textarea>
+                <div className="flex justify-between items-center">
+                    <div className="flex justify-end items-center mb-1 hover:cursor-pointer">
+                        <button onClick={()=>navigate("/",{replace:true})} className="text-3xl md:text-5xl hover:text-red-500 cursor-pointer p-2">&times;</button>
+                    </div>
+                    <div className="flex justify-end hover:cursor-pointer">
+                        <button className="border text-2xl font-bold border-black px-8 py-2 hover:cursor-pointer hover:bg-[#5e8c6b] rounded-2xl" onClick={postingdata}>Post</button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/fetchedlist.jsx
+++ src/components/fetchedlist.jsx
@@ -1,20 +1,50 @@
 import Appendandseedata from "./appendseedata";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 export default function Fetchedlist(){
+    const [data,setdata]=useState(null);
     const [show,setshow]=useState(false);
-    return(
-        <div className="p-4 md:p-10 w-full flex justify-center relative">
-            <div className="w-full max-w-4xl" >
-            <button className="w-full hover:cursor-pointer hover:bg-[#222e22] h-auto px-4 py-3 md:px-6 md:py-4 border-2 border-indigo-500 rounded-2xl text-white bg-[#202420] flex flex-col gap-2 md:gap-3 transition-all" onClick={()=>setshow(true)}>
-                <h1 className="text-2xl md:text-4xl font-bold text-start">This is something</h1>
-                <p className="truncate leading-relaxed text-lg md:text-2xl w-full">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laudantium sapiente ea error eligendi magnam maxime iste facere? Expedita rem ratione tenetur. Pariatur inventore, reiciendis sed dicta ex velit quisquam, nobis sequi quis iure dolores error suscipit consectetur doloribus odit cum. Dolorem perferendis quam aperiam omnis minus rem fugit praesentium!</p>
-            </button>
+    const [objid,setobjid]=useState("");
+    const [objtitle,setobjtitle]=useState("");
+    const [objcontent,setobjcontent]=useState("");
+    
+    const fetchdata=async ()=>{
+        try{
+            const response=await fetch("/api/");
+            const result = await response.json();
+            console.log(result);
+            setdata(result);
             
-            </div>
+        }catch(err){
+            console.log(err);
+        }
+
+    }
+    useEffect(() => {
+      fetchdata();
+    
+      
+    }, [])
+    
+    
+    return(
+        <div className="p-4 md:p-10 w-full flex flex-col items-center relative gap-4">
+            {data && data.map(info=>{return(
+                <div className="w-full max-w-4xl" key={info._id}>
+                    <button className="w-full hover:cursor-pointer hover:bg-[#222e22] h-auto px-4 py-3 md:px-6 md:py-4 border-2 border-indigo-500 rounded-2xl text-white bg-[#202420] flex flex-col gap-2 md:gap-3 transition-all" onClick={()=>{setobjid(info._id); setobjtitle(info.title); setobjcontent(info.content); setshow(true);}}>
+
+                        <h1 className="text-2xl md:text-4xl font-bold text-start">{info.title}</h1>
+                        <p className="truncate leading-relaxed text-lg md:text-2xl w-full text-start"> {info.content}</p>
+                        
+                    </button>
+                
+                </div>)
+            })  
+            }
+
             {show && (
                 <div className="fixed inset-0 z-[9999] bg-[#d0d1b4] flex items-center justify-center p-2 md:p-6">
-                    <Appendandseedata onClose={()=>setshow(false)} />
+                    <Appendandseedata onClose={()=>setshow(false)} objtitle={objtitle} objcontent={objcontent} objid={objid} refresh={fetchdata} />
 
                 </div>
             )
